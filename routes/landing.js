b645040e0608d12ae9b52a4886ae3e5aa5d00e1a
waihouC <changwh5@gmail.com>
2021-11-14 21:30:16 +0000

    update carousel on landing page.

--- routes/landing.js
+++ routes/landing.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+// import moment js
+let moment = require("moment");
+
 // import in the Product model
 const { Product } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('landing/index')
-})
+router.get('/', async (req, res) => {
+    // retrieve top 3 latest products created within 2 months
+    const products = await Product
+        .where('created_on', '>=', moment().subtract(2, 'months').format())
+        .query(function(qb) {
+            qb.limit(3);
+        })
+        .fetchAll();
+
+    res.render('landing/index', {
+        'products': products.toJSON()
+    });
+});
 
 router.get('/about-us', (req, res) => {
-    res.render('landing/about')
-})
+    res.render('landing/about');
+});
 
 module.exports = router;
